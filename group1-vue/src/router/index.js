ec3d9e71b5d014e8a57c1b0f0700a9a1d7a94d4e
Purin18 <2417715@mwu.jp>
2026-07-28 22:08:03 +0900

    indexを変更

--- group1-vue/src/router/index.js
+++ group1-vue/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
-import ChatView from '../chat.vue';
+import ChatView from '../views/chat.vue';
 
 const routes = [
   {
