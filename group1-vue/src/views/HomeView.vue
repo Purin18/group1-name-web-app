d1ad67f4646b2fee8cd01d0dd05e791c7513141d
Purin18 <2417715@mwu.jp>
2026-07-29 10:40:41 +0900

    チャットボタン変更

--- group1-vue/src/views/HomeView.vue
+++ group1-vue/src/views/HomeView.vue
@@ -38,7 +38,7 @@
 
       <!-- ③ メイン機能ボタン（3列） -->
       <div class="action-buttons">
-        <button class="btn main-btn">チャット</button>
+        <button class="btn main-btn" @click="goToChat">チャット</button>
         <button class="btn main-btn">マッチング</button>
         <button class="btn main-btn">グループ作成</button>
       </div>
@@ -52,7 +52,13 @@
 </template>
 
 <script setup>
-// 今後、予定や課題のデータを動的（配列）にする処理をここに書いていきます！
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goToChat = () => {
+  router.push('/chat');
+};
 </script>
 
 <style scoped>
